c7633e1ecd17a725bab5f2affdc9c6dea08db222
Johannes Schickling <schickling.j@gmail.com>
2018-01-02 10:34:14 +0100

    chore: linting

--- src/voyager.ts
+++ src/voyager.ts
@@ -4,7 +4,7 @@ exports.builder = {
   port: {
     alias: 'p',
     description: 'port to start local server with voyager on',
-  }
+  },
 }
 
 import * as express from 'express'
@@ -13,19 +13,22 @@ import * as graphqlHTTP from 'express-graphql'
 import * as opn from 'opn'
 import { buildSchema } from 'graphql'
 
-exports.handler = async function (context, argv) {
+exports.handler = async function(context, argv) {
   const config = await context.getProjectConfig()
   const schema = buildSchema(config.getSchemaSDL())
 
   const app = express()
 
-  app.use('/graphql', graphqlHTTP({
-    schema: schema
-  }));
+  app.use(
+    '/graphql',
+    graphqlHTTP({
+      schema: schema,
+    }),
+  )
 
   app.use('/voyager', middleware({ endpointUrl: '/graphql' }))
 
-  const port = parseInt(argv.port) || 7000;
+  const port = parseInt(argv.port) || 7000
   const listener = app.listen(port, () => {
     let host = listener.address().address
     if (host === '::') {
@@ -34,5 +37,5 @@ exports.handler = async function (context, argv) {
     const link = `http://${host}:${port}/voyager`
     console.log('Serving voyager at %s', link)
     opn(link)
-  });
+  })
 }
